256e5dbb2f9a213935dd23ae6f2a9f0582ca6947
Abdullah Al Muzaki <69757282+aamuzakii@users.noreply.github.com>
2023-10-03 16:22:49 +0700

    Create init_work.sh

---
+++ init_work.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Open Chrome
+google-chrome &
+
+# Open VSCode
+code &
+
+# Open Tilix
+tilix &
+
+# Start CopyQ
+copyq &
+
+# Start Flameshot
+flameshot &
+
+# Open Google Chat in a web browser
+google-chrome https://chat.google.com &

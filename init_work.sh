4e1b80ee5c46c0c8398348d4c10c5468e7bb2fb2
Abdullah Al Muzaki <69757282+aamuzakii@users.noreply.github.com>
2023-10-09 10:29:12 +0700

    Update init_work.sh

--- init_work.sh
+++ init_work.sh
@@ -9,11 +9,24 @@ code &
 # Open Tilix
 tilix &
 
+sleep 3
 # Start CopyQ
-copyq &
+/usr/bin/copyq &
 
 # Start Flameshot
-flameshot &
+# /usr/bin/flameshot gui &
+sleep 10
+google-chat-electron &
 
-# Open Google Chat in a web browser
-google-chrome https://chat.google.com &
+# Check if the tab is already open in Chrome
+while true; do
+    if google-chrome https://hr.talenta.co/employee/dashboard; then
+        notify-send "Talnta tab is ready, breaking the loop"
+        break  # Exit the loop when the tab is ready
+    else
+        echo "Waiting for the tab to be ready..."
+        sleep 3  # Adjust the sleep duration as needed
+    fi
+done
+
+microsoft-edge https://chat.openai.com &
